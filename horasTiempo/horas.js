ab26e4e56c74043e1e31ccf6d912518b115796e1
AnassGI <95426424+AnassGi@users.noreply.github.com>
2022-12-13 19:48:52 +0100

    Add files via upload

---
+++ horasTiempo/horas.js
@@ -0,0 +1,22 @@
+function save(){
+    var hora_Monday = document.getElementById("Monday");
+    localStorage.setItem("Monday",hora_Monday);
+
+    var hora_Tuesday = document.getElementById("Tuesday");
+    localStorage.setItem("Tuesday",hora_Tuesday);
+
+    var hora_wednesday = document.getElementById("wednesday");
+    localStorage.setItem("wednesday",hora_wednesday);
+
+    var hora_thursday = document.getElementById("thursday");
+    localStorage.setItem("thursday",hora_thursday);
+
+    var hora_Friday = document.getElementById("Friday");
+    localStorage.setItem("Friday",hora_Friday);
+
+    var hora_Saturday = document.getElementById("Saturday");
+    localStorage.setItem("Saturday",hora_Saturday);
+
+    var hora_Sunday = document.getElementById("Sunday");
+    localStorage.setItem("Sunday",hora_Sunday);
+}
